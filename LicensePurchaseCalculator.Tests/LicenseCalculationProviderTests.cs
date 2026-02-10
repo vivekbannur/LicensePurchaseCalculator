67a9a019d25f949bdf117e48524b6d51517fbc63
vivekbannur <vivekbsengg@gmail.com>
2026-02-10 12:18:28 +0000

    Basic unit test cases for core license calculation logic

---
+++ LicensePurchaseCalculator.Tests/LicenseCalculationProviderTests.cs
@@ -0,0 +1,51 @@
+using LicensePurchaseCalculator.Implementations.Providers;
+using LicensePurchaseCalculator.Models;
+
+namespace LicensePurchaseCalculator.Tests
+{
+    public class LicenseCalculationProviderTests
+    {
+        private readonly LicenseCalculationProvider _sut = new LicenseCalculationProvider();
+
+        [Fact]
+        public void Example1_UserHasLaptopAndDesktop_Requires1Copy()
+        {
+            var data = new[]
+            {
+                new AppInstallationModel { ComputerID = 1, UserID = 1, ApplicationID = 374, ComputerType = "LAPTOP",  Comment="A" },
+                new AppInstallationModel { ComputerID = 2, UserID = 1, ApplicationID = 374, ComputerType = "DESKTOP", Comment="A" },
+            };
+            var result = _sut.CalculateMinimumLicenses(data, 374);
+            Assert.Equal(1, result);
+        }
+
+        [Fact]
+        public void Example2_TwoUsers_Requires3Copies()
+        {
+            var data = new[]
+            {
+                new AppInstallationModel { ComputerID = 1, UserID = 1, ApplicationID = 374, ComputerType = "LAPTOP",  Comment="A" },
+                new AppInstallationModel { ComputerID = 2, UserID = 1, ApplicationID = 374, ComputerType = "DESKTOP", Comment="A" },
+
+                new AppInstallationModel { ComputerID = 3, UserID = 2, ApplicationID = 374, ComputerType = "DESKTOP", Comment="A" },
+                new AppInstallationModel { ComputerID = 4, UserID = 2, ApplicationID = 374, ComputerType = "DESKTOP", Comment="A" },
+            };
+            var result = _sut.CalculateMinimumLicenses(data, 374);
+            Assert.Equal(3, result);
+        }
+
+        [Fact]
+        public void Example3_DuplicateRows_Requires2Copies()
+        {
+            var data = new[]
+            {
+                new AppInstallationModel { ComputerID = 1, UserID = 1, ApplicationID = 374, ComputerType = "LAPTOP",  Comment="SystemA" },
+
+                new AppInstallationModel { ComputerID = 2, UserID = 2, ApplicationID = 374, ComputerType = "DESKTOP", Comment="SystemA" },
+                new AppInstallationModel { ComputerID = 2, UserID = 2, ApplicationID = 374, ComputerType = "desktop", Comment="SystemB" }, // duplicate per example
+            };
+            var result = _sut.CalculateMinimumLicenses(data, 374);
+            Assert.Equal(2, result);
+        }
+    }
+}
